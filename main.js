190a9b8d153b5876e598a736336aa800b56ace96
Dagmawi <asegda17@gmail.com>
2021-03-30 17:06:08 +0200

    Arithmetic pt.2

---
+++ main.js
@@ -0,0 +1,39 @@
+const ticketPrice = 15;
+let totalPrice = 6450;
+console.log('$' + totalPrice / ticketPrice);
+
+
+const incomePerWeek = 500;
+const numberOfWeeks = 52;
+console.log('$' + incomePerWeek * numberOfWeeks);
+
+console.log((17 * 100 / 30) + '%');
+
+console.log(4.75 * 4 + 'cm');
+
+console.log(5 + 6 + 7 + 'cm');
+
+console.log(9**3 + 'cm^3');
+
+console.log((22.35 + 10 * 22.35 / 100));
+console.log((26.67 + 15 * 26.67 / 100));
+console.log((35.92 + 20 * 35.92 / 100));
+
+console.log((22.35 + 10 * 22.35 / 100) + (26.67 + 15 * 26.67 / 100) + (35.92 + 20 * 35.92 / 100));
+
+const noemyHours = 8+6+5+9+8+2+1+8.5+7+4;
+console.log(noemyHours / 10);
+
+const score = 75 + 70 +  85 + 90 + 100;
+const x = 85 * 6 - score;
+console.log(`Score in the sixth test: ${x}`);
+
+//Q12
+const totalScoreOfEightTest = 78 * 8;
+const totalScoreOfNineTest = 80 * 9;
+console.log('James needs a minimum of ' + totalScoreOfEightTest * 100 /totalScoreOfNineTest + '%');
+
+
+
+
+
